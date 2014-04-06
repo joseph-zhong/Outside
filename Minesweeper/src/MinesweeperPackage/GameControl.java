c0929c4c638b803610ef922f7242cc42bfeebeec
Joseph Zhong <glub.bob@gmail.com>
2014-04-05 21:44:31 -0700

    Added my old (mid-10th grade?) Calculator Project
    A Microsoft Calculator-inspired project
    Added Project files

---
+++ Minesweeper/src/MinesweeperPackage/GameControl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MinesweeperPackage;
+
+/**
+ *
+ *  Joseph Zhong
+ *  ASSIGNMENT_NUMBER
+ *  PROGRAM_DESCRIPTION
+ *  PROGRAM_TITLE
+ *  DATE
+ *
+ **/
+
+public class GameControl 
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
