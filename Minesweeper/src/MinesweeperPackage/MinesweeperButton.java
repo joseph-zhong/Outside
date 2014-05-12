f4b9099940c30a242789704849e2d1c29f118ebb
Joseph Zhong <glub.bob@gmail.com>
2014-05-11 21:55:41 -0700

    Added MinesweeperButton object
    flagged seems to be state that needs to be there...

---
+++ Minesweeper/src/MinesweeperPackage/MinesweeperButton.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MinesweeperPackage;
+
+import javax.swing.AbstractButton;
+
+/**
+ *
+ *  Joseph Zhong
+ *  ASSIGNMENT_NUMBER
+ *  PROGRAM_DESCRIPTION
+ *  PROGRAM_TITLE
+ *  DATE
+ *
+ **/
+
+public class MinesweeperButton extends AbstractButton
+{
+    
+}
