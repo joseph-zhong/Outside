cb0b0d7ca51dd03d59939a3c3e6216c6b424072d
Joseph Zhong <glub.bob@gmail.com>
2014-05-30 00:31:26 -0700

    Created internalLogic package

--- Minesweeper/src/InternalLogic/GameControl.java
+++ Minesweeper/src/InternalLogic/GameControl.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 
-package MinesweeperPackage;
+package InternalLogic;
 
 import java.util.Scanner;
 
--- Minesweeper/src/InternalLogic/Setting.java
+++ Minesweeper/src/InternalLogic/Setting.java
@@ -3,43 +3,43 @@
  * and open the template in the editor.
  */
 
-package MinesweeperPackage;
+package InternalLogic;
 
 /**
  *
  *  Joseph Zhong
  *  Minesweeper - Java (v2.5)
  *  This program is the Setting Object for my overall Minesweeper Project
- *  Minesweeper - Setting Object 
+ *  Minesweeper - Setting Object
  *  1 April 2014
  *
  **/
 
-public class Setting 
+public class Setting
 {
     private int mines;
-    
+
     private int xMax;
-    
+
     private int yMax;
-    
+
     public Setting(int _mines, int _xMax, int _yMax)
     {
         mines = _mines;
         xMax = _xMax;
         yMax = _yMax;
     }
-    
+
     public int getMines()
     {
         return mines;
     }
-    
+
     public int getX()
     {
         return xMax;
     }
-    
+
     public int getY()
     {
         return yMax;
